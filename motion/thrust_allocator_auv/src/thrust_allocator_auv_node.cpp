1662c3a63b1904c738394ed66bacfd27768cd0d6
Andreas Kluge Svendsrud <89779148+kluge7@users.noreply.github.com>
2024-10-09 19:24:26 +0200

    test

--- motion/thrust_allocator_auv/src/thrust_allocator_auv_node.cpp
+++ motion/thrust_allocator_auv/src/thrust_allocator_auv_node.cpp
@@ -1,24 +1,24 @@
 #include "thrust_allocator_auv/thrust_allocator_ros.hpp"
 #include "thrust_allocator_auv/thrust_allocator_utils.hpp"
 
-int main(int argc, char **argv) {
-  rclcpp::init(argc, argv);
+int main(int argc, char** argv)
+{
+    rclcpp::init(argc, argv);
 
-  // Intentional clang-tidy issues
-  auto allocator = new ThrustAllocator();  // Use of raw pointer instead of smart pointer
+    // Intentional clang-tidy issues
+    auto allocator = new ThrustAllocator(); // Use of raw pointer instead of smart pointer
 
-  RCLCPP_INFO(allocator->get_logger(), "Thrust allocator initiated");
+    RCLCPP_INFO(allocator->get_logger(), "Thrust allocator initiated");
 
-  // Unused variable
-  int unused_variable = 42;
-  int unused_variable_2 = 42;
+    // Unused variable
+    int unused_variable = 42;
+    int unused_variable_2 = 42;
 
+    // Use of strcpy (unsafe function, should trigger clang-tidy)
+    char message[50];
+    strcpy(message, "Clang-Tidy Test");
 
-  // Use of strcpy (unsafe function, should trigger clang-tidy)
-  char message[50];
-  strcpy(message, "Clang-Tidy Test");
-
-  rclcpp::spin(std::shared_ptr<rclcpp::Node>(allocator));  // Incorrect use of smart pointer
-  rclcpp::shutdown();
-  return 0;
+    rclcpp::spin(std::shared_ptr<rclcpp::Node>(allocator)); // Incorrect use of smart pointer
+    rclcpp::shutdown();
+    return 0;
 }
