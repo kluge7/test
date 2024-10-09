bfdfd025fc010c9f231095a2a54e15e31e80c30a
Andreas Kluge Svendsrud <89779148+kluge7@users.noreply.github.com>
2024-10-10 01:44:38 +0200

    test

--- motion/thrust_allocator_auv/include/thrust_allocator_auv/pseudoinverse_allocator.hpp
+++ motion/thrust_allocator_auv/include/thrust_allocator_auv/pseudoinverse_allocator.hpp
@@ -30,7 +30,7 @@ public:
      * @param tau The input torques as a vector.
      * @return The allocated thrust as a vector.
      */
-    Eigen::VectorXd calculate_allocated_thrust(const Eigen::VectorXd& tau);
+    auto calculate_allocated_thrust(const Eigen::VectorXd& tau) -> Eigen::VectorXd;
 
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
     Eigen::MatrixXd T_pinv;
--- motion/thrust_allocator_auv/src/pseudoinverse_allocator.cpp
+++ motion/thrust_allocator_auv/src/pseudoinverse_allocator.cpp
@@ -1,12 +1,11 @@
 #include "thrust_allocator_auv/pseudoinverse_allocator.hpp"
 
-PseudoinverseAllocator::PseudoinverseAllocator(const Eigen::MatrixXd& T_pinv)
-    : T_pinv(T_pinv)
+PseudoinverseAllocator::PseudoinverseAllocator(Eigen::MatrixXd T_pinv)
+    : T_pinv_(std::move(T_pinv))
 {
 }
 
-Eigen::VectorXd
-PseudoinverseAllocator::calculate_allocated_thrust(const Eigen::VectorXd& tau)
+Eigen::VectorXd auto PseudoinverseAllocator::calculate_allocated_thrust(const Eigen::VectorXd& tau) -> Eigen::VectorXd
 {
     Eigen::VectorXd u = T_pinv * tau;
     return u;
