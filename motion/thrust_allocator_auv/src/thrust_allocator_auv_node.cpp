83d9477fdb53e69f623790a2bbb97b118f67a661
Andreas Kluge Svendsrud <kluge7@users.noreply.github.com>
2024-11-09 22:34:10 +0100

    test

--- motion/thrust_allocator_auv/src/thrust_allocator_auv_node.cpp
+++ motion/thrust_allocator_auv/src/thrust_allocator_auv_node.cpp
@@ -1,3 +1,6 @@
+#include "thrust_allocator_auv/thrust_allocator_ros.hpp"
+#include "thrust_allocator_auv/thrust_allocator_utils.hpp"
+
 int main(int argc, char **argv) {
   rclcpp::init(argc, argv);
   auto allocator = std::make_shared<ThrustAllocator>();
